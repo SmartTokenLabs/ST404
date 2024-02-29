025c8e6463c470e3571300b0956b400d71a435c7
Oleh Hryb <oleg.grib.ua@gmail.com>
2024-02-29 06:00:42 +0000

    feat: 🎸 hardhat config sepolia, mumbai

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,20 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  networks: {
+    // "https://rpc-mumbai.maticvigil.com",
+    // "https://polygon-mumbai-bor.publicnode.com",
+    // "wss://polygon-mumbai-bor.publicnode.com",
+    // "https://polygon-mumbai.gateway.tenderly.co",
+    // "wss://polygon-mumbai.gateway.tenderly.co"
+    // "https://matic-mumbai.chainstacklabs.com"
+    polygonMumbai: {
+      url: `https://rpc-mumbai.maticvigil.com`
+    },
+    sepolia: {
+      url: `https://rpc.sepolia.org`
+    }
+  }
 };
 
 export default config;
